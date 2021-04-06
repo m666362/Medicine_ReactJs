f96c3266121e5a4485eb246545062d358579f1d8
github_username <mdrayhan9464@gmail.com>
2021-04-06 20:00:10 +0600

    Updates

---
+++ precache-manifest.66343a67e1c8675be0d9f849b4ac1f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d5b1a257ff7b196ee579f36921f9ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e684860780fba3635cf9",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "218efc983affa8848df5",
+    "url": "/static/js/2.9e5a4679.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9e5a4679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e684860780fba3635cf9",
+    "url": "/static/js/main.0f0e5125.chunk.js"
+  },
+  {
+    "revision": "154719c7715aa511bab2",
+    "url": "/static/js/runtime-main.7ce21181.js"
+  }
+]);
