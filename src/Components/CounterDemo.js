e2b364f070ebca775964da6b1599c392256ba25f
github_username <mdrayhan9464@gmail.com>
2021-04-06 19:56:21 +0600

    initial commit

---
+++ src/Components/CounterDemo.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import Grid from "@material-ui/core/Grid";
+import {useOvermind} from "../Others/OvermindHelper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {makeStyles} from '@material-ui/core/styles';
+const {FayFetch} = require('fayfetch')
+
+const useStyles = makeStyles((theme) => ({
+    //
+}))
+
+const CounterDemo = () => {
+    const {state, actions} = useOvermind()
+    const classes = useStyles();
+
+    useEffect(() => {
+        // FayFetch.get('https://api.npoint.io/6db6479a282d181d7ceb', null, null, (status, data, ok) => {
+        //     console.log("OK: " + ok)
+        // })
+    }, [])
+
+    return (
+        <Grid style={{padding: 48}} container direction='column' justify='center' alignItems='center'
+              alignContent='center'>
+            <Typography>{state.counter}</Typography>
+            <Grid container direction='row' alignItems='center' alignContent='center' justify='center'>
+                <Button style={{margin: 8}} color='primary' variant='contained' onClick={() => {
+                    actions.increase(1)
+                }}>+</Button>
+                <Button style={{margin: 8}} color='primary' variant='contained' onClick={() => {
+                    actions.increase(-1)
+                }}>-</Button>
+            </Grid>
+
+        </Grid>
+    );
+};
+
+export default CounterDemo;
